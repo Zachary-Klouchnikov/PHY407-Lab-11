f2e055896e902e1a55665a1ee30fbbf386a6316b
Zachary Klouchnikov <zachary.klou@gmail.com>
2025-11-26 13:03:32 -0500

    Completed question 3

--- Lab11_Q3.py
+++ Lab11_Q3.py
@@ -1,13 +1,15 @@
 __authors__ = "Zachary Klouchnikov and Hannah Semple"
 
-# HEADER
+# The following code performs simulated annealing to solve two problems:
+# Part A) The Traveling Salesman Problem (TSP) for 25 cities with different cooling schedules
+# Part B) Optimization of a given function using simulated annealing techniques.
+# Code from Part A) use code from salesman.py provided by Newman.
 
 """
 IMPORTS
 """
 import random
 import numpy as np
-import vpython as vp
 import matplotlib.pyplot as plt
 
 plt.rc('text', usetex = True)
@@ -35,6 +37,14 @@ def distance():
         s += mag(r[i + 1] - r[i])
 
     return s
+
+def gaussian():
+    """Returns a random number drawn from a Gaussian distribution.
+    """
+    x = np.random.rand()
+    y = np.random.rand()
+
+    return np.sqrt(-2 * np.log(x)) * np.cos(2 * np.pi * y)
 
 """
 PART A)
@@ -182,5 +192,124 @@ plt.grid()
 # Limits
 plt.xlim(1, 5)
 
-plt.savefig('Figures\\Simulated Annealing Optimization.pdf')
+# plt.savefig('Figures\\Simulated Annealing Optimization.pdf')
+plt.show()
+
+"""
+PART B)
+"""
+"Constants and defining functions"
+T_0 = 1.0
+T_MIN = 1e-3
+TAU = 1e4
+
+# Equation (10)
+f = lambda x, y: (x ** 2) - np.cos(4 * np.pi * x) + (y - 1) ** 2
+# Equation (12)
+g = lambda x, y: np.cos(x) + np.cos(np.sqrt(2) * x) + np.cos(np.sqrt(3) * x) + (y - 1) ** 2
+
+"Simulated annealing to minimize equation (10)"
+t = 0
+x = 2.0
+y = 2.0
+
+path_x = []
+path_y = []
+path_f = []
+
+t_index = T_0
+while t_index > T_MIN:
+    path_x.append(x)
+    path_y.append(y)
+    path_f.append(f(x, y))
+
+    # Cooling
+    t += 1
+    t_index = T_0 * np.exp(-t / TAU)
+
+    # Propose Monte Carlo step
+    dx = gaussian()
+    dy = gaussian()
+    x_new = x + dx
+    y_new = y + dy
+
+    # Energy difference
+    dE = f(x_new, y_new) - f(x, y)
+
+    # Metropolis probability
+    if dE < 0 or np.random.rand() < np.exp(-dE / t_index):
+        x, y = x_new, y_new
+
+print(f"Final position: x = {x}, y = {y}")
+
+"Plotting Simulated Annealing Optimization of Equation (10)"
+plt.figure()
+
+# Plotting simulated annealing optimization of Equation (10)
+plt.scatter(path_x, path_y, c = np.arange(len(path_x)), cmap = 'viridis', s = 4)
+
+# Labels
+plt.title("Simulated Annealing Optimization of Equation (10)", fontsize = 12)
+plt.xlabel("x", fontsize = 12)
+plt.ylabel("y", fontsize = 12)
+
+plt.colorbar(label = "Step Index")
+plt.grid()
+
+# plt.savefig('Figures\\Simulated Annealing Optimization of Equation (10).pdf')
+plt.show()
+
+"Simulated annealing to minimize equation (12)"
+t = 0
+x = 2.0
+y = 2.0
+
+path_x = []
+path_y = []
+path_g = []
+
+t_index = T_0
+while t_index > T_MIN:
+    path_x.append(x)
+    path_y.append(y)
+    path_g.append(g(x, y))
+
+    # Cooling
+    t += 1
+    t_index = T_0 * np.exp(-t / TAU)
+
+    # Propose Monte Carlo step
+    dx = gaussian()
+    dy = gaussian()
+    x_new = x + dx
+    y_new = y + dy
+
+    # Reject step if outside domain
+    if not (0 < x_new < 50 and -20 < y_new < 20):
+        continue
+
+    # Energy difference
+    dE = g(x_new, y_new) - g(x, y)
+
+    # Metropolis probability
+    if dE < 0 or np.random.rand() < np.exp(-dE / t_index):
+        x, y = x_new, y_new
+
+print(f"Final position: x = {x}, y = {y}")
+
+"Plotting Simulated Annealing Optimization of Equation (12)"
+plt.figure()
+
+# Plotting simulated annealing optimization of Equation (12)
+plt.scatter(path_x, path_y, c = np.arange(len(path_x)), cmap = 'viridis', s = 4)
+
+# Labels
+plt.title("Simulated Annealing Optimization of Equation (12)", fontsize = 12)
+plt.xlabel("x", fontsize = 12)
+plt.ylabel("y", fontsize = 12)
+
+plt.colorbar(label = "Step Index")
+plt.grid()
+
+# plt.savefig('Figures\\Simulated Annealing Optimization of Equation (12).pdf')
 plt.show()
